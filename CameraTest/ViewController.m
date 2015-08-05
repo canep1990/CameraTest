b06882f8623edc45b73d042c5d0a051ce259d045
Yury Voskresensky <yriy.voskresenskiy@arsenalmedia.ru>
2015-08-05 12:05:29 +1000

    added video capturing

--- CameraTest/ViewController.m
+++ CameraTest/ViewController.m
@@ -7,31 +7,91 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
 
-@interface ViewController ()
+/** Constant for size offset */
+CGFloat const kSizeOffset = 60;
+
+/** Constant for start view title */
+NSString * const kVideoStartTitle = @"Start";
+
+/** Constant for stop view title */
+NSString * const kVideoStopTitle = @"Stop";
+
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+
+/** Button to display video */
+@property (weak, nonatomic) IBOutlet UIButton *videoButton;
+
+/** Current device */
+@property (weak, nonatomic) AVCaptureDevice *currentDevice;
+
+/** The capture session takes the input from the camera and capture it */
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+
+/** Layer for displaying video */
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (IBAction)startVideo:(id)sender
+{
+    if (self.captureSession.running)
+    {
+        [self.captureSession stopRunning];
+        [self.videoButton setTitle:kVideoStartTitle forState:UIControlStateNormal];
+    }
+    else
+    {
+        [self.videoButton setTitle:kVideoStopTitle forState:UIControlStateNormal];
+        [self startVideo];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)startVideo
+{
+    self.captureSession = [[AVCaptureSession alloc] init];
+    self.captureSession.sessionPreset = AVCaptureSessionPresetHigh;
+    NSArray *devicesArray = [AVCaptureDevice devices];
+    if (devicesArray && devicesArray.count > 0)
+    {
+        for (AVCaptureDevice *device in devicesArray)
+        {
+            if ([device hasMediaType:AVMediaTypeVideo])
+            {
+                if(device.position == AVCaptureDevicePositionBack)
+                {
+                    self.currentDevice = device;
+                    [self showVideo];
+                }
+            }
+        }
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)showVideo
+{
+    NSError *error = nil;
+    [self.captureSession addInput:[[AVCaptureDeviceInput alloc] initWithDevice:self.currentDevice error:&error]];
+    if (!error)
+    {
+        self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+        [self.view.layer addSublayer:self.previewLayer];
+        CGRect frame = self.view.layer.frame;
+        frame.origin.y = kSizeOffset;
+        frame.size.height = frame.size.height - kSizeOffset;
+        self.previewLayer.frame = frame;
+        [self.captureSession startRunning];
+    }
+    else
+    {
+        NSLog(@"error: %@", error.localizedDescription);
+    }
 }
-*/
 
 @end
