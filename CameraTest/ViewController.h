d2cf7c82a351024143bb5eeadfe3aaa51984bd01
Yury Voskresensky <yriy.voskresenskiy@arsenalmedia.ru>
2015-08-05 11:20:50 +1000

    added User interface

---
+++ CameraTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraTest
+//
+//  Created by Юрий Воскресенский on 05.08.15.
+//  Copyright (c) 2015 Юрий Воскресенский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
